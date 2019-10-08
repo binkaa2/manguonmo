c6abefa369948ef3a1be272e0519f42b78bf3782
thanhk40a <nguyenvanthanhk40a@gmail.com>
2019-10-08 08:18:38 +0700

    mr dung

--- baiso5.php
+++ baiso5.php
@@ -1,119 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<?php include_once("model/book.php");
-    $search = '';
-    if(isset($_GET['search']))
-      $search = $_GET['search'];
-    $books = Book::getList($search);
+<?php 
+    include_once('model/sinhvien.php');
+    $listSV = SinhVien::getList();
 ?>
-
-<head>
-    <?php include_once("includes/header.php");  ?>
-</head>
-
-<body id="page-top">
-    
-    <?php include_once("includes/nav.php") ?>
-
-    <div id="wrapper">
-        
-        <?php include_once("includes/sidebar.php") ?>
-
-        <div id="content-wrapper">
-
-<div class="container-fluid">
-
-  <!-- Breadcrumbs-->
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item">
-      <a href="#">Dashboard</a>
-    </li>
-    <li class="breadcrumb-item active">Tables</li>
-  </ol>
-
-  <!-- DataTables Example -->
-  <div class="card mb-3">
-    <div class="card-header">
-      <i class="fas fa-table"></i>
-    Bai tap thay dung 5</div>
-    <div class="card-body">
-    <div class="row float-left">
-      <div class="col">
-      <form method="GET" action="">
-        <input class="form-control" type="search" placeholder="Search" name="search" />
-      </form>
-      </div>
-    </div>
-    <div class="row float-right">
-        <div class="col pb-3">
-        <button class="btn" style="border: 1px solid #6170DF;
-    font-family: Avenir-Heavy;
-    font-size: 13px;
-    color: #6170DF;
-    line-height: 20px;">+ Add</button>
+<?php include_once('header.php') ?>
+<?php include_once('nav.php') ?>
+<?php include_once('dropdown.php') ?>
+<?php 
+    if(!empty($_REQUEST['id'])){
+        if(strcmp($_REQUEST['action'],"xoa")==0){
+            unset($listSV[$_REQUEST['id']-1]);
+            unset($_SESSION['list'][$_REQUEST['id']-1]);
+            $_SESSION['list']=$listSV;
+        }
+    }
+?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <button class="btn btn-primary">Thêm</button>
+            </div>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead align="center">
+                    <tr style="background: #333;color:#fff">
+                        <th>STT</th>
+                        <th>Từ năm</th>
+                        <th>Tên</th>
+                        <th>Đến năm</th>
+                        <th>Lớp</th>
+                        <th>Nơi học</th>
+                        <th colspan="2" style="text-align:center">Thao tác</th>
+                    </tr>
+                </thead>
+                <tbody align="center">
+                    <?php
+                        foreach ($listSV as $key => $value) {
+                    ?>         
+                        <tr>
+                            <td><?php echo $value->id ?></td>
+                            <td><?php echo $value->name ?></td>
+                            <td><?php echo $value->to ?></td>
+                            <td><?php echo $value->from ?></td>
+                            <td><?php echo $value->class ?></td>
+                            <td><?php echo $value->address ?></td>
+                            <td>
+                                <a href="baiso5.php?id=<?php echo $value->id ?>&action=sua" class="btn btn-outline-success"><i class="fas fa-edit"></i>Sửa</a> 
+                            </td>
+                            <td>
+                                <a href="baiso5.php?id=<?php echo $value->id ?>&action=xoa" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i>Xóa</a> 
+                            </td>
+                        </tr>
+                        
+                    <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
-      <div class="table-responsive">
-        <table class="table table-bordered" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              <th style="background: black;
-    color: white;">ID</th>
-              <th style="background: black;
-    color: white;">Title</th>
-              <th style="background: black;
-    color: white;">Author</th>
-              <th style="background: black;
-    color: white;">YEAR</th>
-              <th style="background: black;
-    color: white;">PRICE</th>
-              <th style="background: black;
-    color: white;">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php 
-            foreach($books as $key => $book){ 
-           ?>
-            <tr>
-              <td><?php echo $key ?></td>
-              <td><?php echo $book->title ?></td>
-              <td><?php echo $book->author ?></td>
-              <td><?php echo $book->year ?></td>
-              <td><?php echo $book->price ?></td>
-              <td width="200px;">
-                  <button class="btn btn-primary">Edit</button>
-                  <button class="btn btn-danger">Delete</button>
-              </td>
-            </tr>
-            <?php
-            }?>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-  </div>
-
-  <p class="small text-center text-muted my-5">
-    <em>More table examples coming soon...</em>
-  </p>
-
-</div>
-<!-- /.container-fluid -->
+<?php include_once('footer.php') ?>
 
 
-</div>
-
-    </div>
-    <!-- /#wrapper -->
-
-    <?php include_once("includes/footer.php")
-  ?>
-
-
-
-</body>
-
-</html>
--- model/book.php
+++ model/book.php
@@ -1,50 +1,82 @@
-
 <?php
-    class Book{
-        #region Properties
-        var $price;
-        var $title;
-        var $author;
-        var $year;
-        #endregion
-
-        #constructor function
-        function __construct($price,$title,$author,$year){
-            $this->price = $price;
-            $this->title = $title;
-            $this->author = $author;
-            $this->year = $year;
-        }
-
-        #Member function
-        function display(){
-            echo "price : ".$this->price."\n"."title : ".$this->title;
-        }
-
-        public static function getList($search){
-            $list = array();
-            array_push($list,new Book(10,'title1','thanh','1999'));
-            array_push($list,new Book(20,'title2','thanh','1997'));
-            array_push($list,new Book(30,'title3','thanh','1998'));
-            array_push($list,new Book(40,'title4','thanh','1996'));
-            array_push($list,new Book(50,'title5','thanh','1995'));
-            array_push($list,new Book(60,'title6','thanh','1994'));
-            array_push($list,new Book(70,'title7','thanh','1993'));
-
-            if($search != ""){
-                foreach($list as $key => $ls){
-                    $check = false;
-                    if(strpos($ls->price,$search) !== false 
-                        || strpos($ls->title,$search) !== false
-                        || strpos($ls->author,$search) !== false
-                        || strpos($ls->year,$search) !== false){
-                        $check = true;
-                    }
-                    if(!$check){
-                        unset($list[$key]);
-                    }
-                }
-            }   
-            return $list;
-        }
+class Book{
+    #--> Properties
+    var $id;
+    var $price;
+    var $title;
+    var $author;
+    var $year;
+    #end properties
+    #Contruct function
+    function __construct($id,$price,$title,$author,$year) {
+        $this->id = $id;
+        $this->price = $price;
+        $this->title = $title;
+        $this->author = $author;
+        $this->year = $year;
     }
+
+    #Member function
+    function display(){
+        echo "Price: ". $this->price. " - Title : ".$this->title."</br>";
+        echo "Author: ".$this->author." - Year: ".$this->year;
+    }
+
+    #Mock data
+    /**
+     * lấy toàn bộ các cuốn sách có trong csdl
+     */
+    static function getList(){
+        $listbook = array();
+        array_push($listbook,new Book(1,55,"PHP ","tuan",2019));
+        array_push($listbook,new Book(2,4,"Ruby ","tuan2",2019));
+        array_push($listbook,new Book(3,41,"Java ","tuan2",2019));
+        array_push($listbook,new Book(4,55,"C# ","tuane",2019));
+
+        return $listbook;
+    }
+    /**
+     * get data to file
+     * 
+     */
+    static function getListFromFile(){
+        $arrData = file("./data/book.txt");
+        $listbook = array();
+        foreach ($arrData as $key => $value) {
+            $arrItem = explode("#",$value);
+            $book = new Book($arrItem[0],$arrItem[2],$arrItem[1],$arrItem[3],$arrItem[4]);
+            array_push($listbook,$book);
+        }  
+        return $listbook;
+    }
+    static function addToFile($value){
+        $myfile = fopen("./data/book.txt", "a") or die("Unable to open file!");
+        fwrite($myfile, $value."\n");
+        fclose($myfile);
+    }
+    static function deleteItem($arr,$id){
+        $myfile = fopen("./data/book.txt", "w") or die("Unable to open file!");
+        unset($arr[$id]);
+        foreach ($arr as $key => $value) {
+            $str = $value->id."#".$value->title."#".$value->price."#".$value->author."#".$value->year;
+            fwrite($myfile, $str);
+        }
+        fclose($myfile);
+    }
+    static function editItem($newItem,$arr){
+        $myfile = fopen("./data/book.txt", "w") or die("Unable to open file!");
+        foreach ($arr as $key => $value) {
+            if($value->id === $newItem->id){
+                $value->title = $newItem->title;
+                $value->price = $newItem->price;
+                $value->author = $newItem->author;
+                $value->year = $newItem->year;    
+            }
+            $str = $value->id."#".$value->title."#".$value->price."#".$value->author."#".$value->year;
+            fwrite($myfile, $str);
+        }
+        fclose($myfile);
+    }
+}
+
+?>
